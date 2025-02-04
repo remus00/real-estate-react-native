f4b8d5287ae6d1e08c2ca760c5856afd5fd48f5b
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-02-04 15:00:23 +0100

    add tabs navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function Index() {
@@ -11,10 +10,6 @@ export default function Index() {
             }}
         >
             <Text className="font-bold text-3xl my-10 font-rubik">Welcome to ReState</Text>
-            <Link href="/sign-in">Sign in</Link>
-            <Link href="/explore">Explore</Link>
-            <Link href="/profile">Profile</Link>
-            <Link href="/properties/1">Example properties</Link>
         </View>
     );
 }
