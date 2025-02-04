e8b1a72ee6ac30592f896a10ac0ded2b508fed9f
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-02-04 13:31:17 +0100

    setup all the assets and fonts

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
                 alignItems: 'center',
             }}
         >
-            <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+            <Text className="font-bold text-3xl my-10 font-rubik">Welcome to ReState</Text>
             <Link href="/sign-in">Sign in</Link>
             <Link href="/explore">Explore</Link>
             <Link href="/profile">Profile</Link>
